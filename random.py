0b312de2b05dbdd17590ef1ea689ddc916a1499d
zeyang77 <146487016+zeyang77@users.noreply.github.com>
2023-10-20 15:54:28 -0500

    Add files via upload

---
+++ random.py
@@ -0,0 +1,22 @@
+import random
+
+diceRoll = input('How many rolls? ')
+diceSides = input('How many sides of the die? ')
+#2&3 asking for number of rolls and sides
+
+diceRoll = int(diceRoll)
+diceSides = int(diceSides)
+#1 dice rolls and sides are intigers
+
+for i in range(diceRoll):
+    randomDice = random.randrange(diceSides)+1
+#4 rolling dice ^
+    print (randomDice)
+#5 dice output
+    if randomDice==20 and diceSides==20:
+        print('critical hit')
+    if randomDice==1 and diceSides==20:
+        print('critical fail')
+#6&7 checking for crit miss and fails
+
+print ('done rolling')
